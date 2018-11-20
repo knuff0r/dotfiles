7f873993f1c7f9033ccd22ba66d98c4d008593ec
knuff0r <wastl.knauer@t-online.de>
2018-11-20 19:17:50 +0100

    mailspring

--- i3/.i3/scripts/dualwp.sh
+++ i3/.i3/scripts/dualwp.sh
@@ -2,4 +2,4 @@
 num=$(ls -l ~/Pictures/Wallpaper/left/ | wc -l)
 ((num--))
 rnd=$(shuf -i 1-$num -n 1)
-feh --bg-scale ~/Pictures/Wallpaper/left/$rnd.jpg --bg-scale ~/Pictures/Wallpaper/right/$rnd.jpg
+feh --bg-scale ~/Wallpaper/left/$rnd.jpg --bg-scale ~/Wallpaper/right/$rnd.jpg
